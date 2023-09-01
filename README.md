2d7d50050153b00383a0acab5d089ade6697a023
remnantofcliff <ville.nieminen0@gmail.com>
2023-09-01 22:34:47 +0300

    First commit, untested on non-Linux OS

---
+++ README.md
@@ -0,0 +1,4 @@
+# gt - get_time - a simple c library to get the current high resolution time 
+
+Provides the ´get_time´-function to get the current high resolution time in
+seconds. Should work on Linux / Mac / Unix / Windows.
---
+++ include/gt/get_time.h
@@ -0,0 +1,22 @@
+#ifdef __unix__
+#include <sys/time.h>
+#elifdef WIN32
+#include <windows.h>
+#endif
+
+static inline double gt_get_time() {
+#ifdef __unix__
+  struct timeval t;
+
+  gettimeofday(&t, nullptr);
+
+  return ((double)t.tv_sec + ((double)t.tv_usec) / 1'000'000.0);
+#elifdef WIN32
+  LARGE_INTEGER freq, t;
+
+  QueryPerformanceCounter(&t);
+  QueryPerformanceFrequency(&freq);
+
+  return t.QuadPart / freq.QuadPart;
+#endif
+}
